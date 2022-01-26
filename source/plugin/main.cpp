4d2ab627fd170c3b4168efea01400bb51e903261
alextwothousand <infinityen@protonmail.com>
2022-01-26 17:54:43 +0000

    same structure as southclaws

--- source/plugin/main.cpp
+++ source/plugin/main.cpp
@@ -1,2 +1,39 @@
 #include <amx/amx2.h>
 #include <plugincommon.h>
+
+#include "common.hpp"
+#include "natives.hpp"
+
+logprintf_t logprintf;
+
+PLUGIN_EXPORT unsigned int PLUGIN_CALL Supports()
+{
+    return SUPPORTS_VERSION | SUPPORTS_AMX_NATIVES;
+}
+
+PLUGIN_EXPORT bool PLUGIN_CALL Load(void** ppData)
+{
+    pAMXFunctions = ppData[PLUGIN_DATA_AMX_EXPORTS];
+    logprintf = (logprintf_t)ppData[PLUGIN_DATA_LOGPRINTF];
+    return true;
+}
+
+extern "C" const AMX_NATIVE_INFO native_list[] = {
+    { "MyFunction", Natives::MyFunction },
+    { NULL, NULL }
+};
+
+PLUGIN_EXPORT int PLUGIN_CALL AmxLoad(AMX* amx)
+{
+    return amx_Register(amx, native_list, -1);
+}
+
+PLUGIN_EXPORT int PLUGIN_CALL Unload()
+{
+    return 1;
+}
+
+PLUGIN_EXPORT int PLUGIN_CALL AmxUnload(AMX* amx)
+{
+    return 1;
+}
---
+++ source/plugin/natives.hpp
@@ -0,0 +1,18 @@
+/*
+# natives.hpp
+Contains all the `PAWN_NATIVE_DECL` for native function declarations.
+*/
+
+#ifndef PROJECTNAME_NATIVES_H
+#define PROJECTNAME_NATIVES_H
+
+#include <string>
+#include <amx/amx2.h>
+
+#include "common.hpp"
+
+namespace Natives {
+cell MyFunction(AMX* amx, cell* params);
+}
+
+#endif
